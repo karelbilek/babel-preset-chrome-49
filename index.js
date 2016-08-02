191284d397dd15ad0e21315b39912ddbbaa2da8b
Karel Bílek <kb@karelbilek.com>
2016-08-02 20:47:38 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  plugins: [
+    
+    require("babel-plugin-transform-es2015-function-name"),
+    require("babel-plugin-transform-es2015-for-of"),
+    require("babel-plugin-transform-es2015-sticky-regex"),
+    require("babel-plugin-transform-es2015-unicode-regex"),
+    require("babel-plugin-transform-es2015-parameters"),
+    require("babel-plugin-transform-es2015-destructuring"),
+    require("babel-plugin-transform-es2015-modules-commonjs")
+  ]
+};
